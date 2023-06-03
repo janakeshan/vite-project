2d8ed3433b57a3273df3fd352af304e5cbb9739a
Janak Eshan <janakeshan@gmail.com>
2023-06-03 16:40:33 +0530

    Test Commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <h2>Hey Janak</h2>
         <button onClick={() => setCount((count) => count + 1)}>
           you clicked {count} times
         </button>
