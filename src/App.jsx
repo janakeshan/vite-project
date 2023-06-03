290834c6385f7afa15bf481878d81cf6eaac8b87
Janak Eshan <janakeshan@gmail.com>
2023-06-03 16:12:31 +0530

    Test 01

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          you clicked {count} times.
+          you clicked {count} times
         </button>
 
       </div>
